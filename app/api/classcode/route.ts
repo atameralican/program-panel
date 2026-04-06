97aaade061c0a3619447efc9aa5a85f970315f0d
Alican ATAMER <atameralican@gmail.com>
2026-04-06 19:46:00 +0300

    classcode page backend completed

--- app/api/classcode/route.ts
+++ app/api/classcode/route.ts
@@ -6,9 +6,53 @@ export async function GET(req: Request) {
     const supabase = await createSupabaseServerClient();
     const { data, error } = await supabase
         .from('classcodes')
-        .select(`*`)
+        .select(`*,modules(id,code),classrooms(id,name)`)
     if (error) {
         return NextResponse.json({ error: error.message }, { status: 500 })
     }
     return NextResponse.json(data, { status: 200 })
+}
+
+export async function POST(req: Request) {
+    const supabase = await createSupabaseServerClient();
+    const { id, ...body } = await req.json();
+    const { data, error } = await supabase
+        .from('classcodes')
+        .insert(body)
+        .select('id,full_name')
+        .single()
+    if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+    return NextResponse.json({message: "Saved!",...data}, { status: 201 })
+}
+
+
+export async function PUT(req:Request){
+    const supabase =await createSupabaseServerClient();
+    const {id,...body}=await req.json();
+    const {data,error}=await supabase
+    .from('classcodes')
+    .update(body)
+    .select()
+    .eq('id',id)
+    .single()
+    if (error) {
+        return NextResponse.json({error:error.message},{status:500})
+    }
+    return NextResponse.json({message:'Updated!',...data},{status:200})   
+}
+
+export async function DELETE(req:Request){
+    const supabase =await createSupabaseServerClient();
+    const {id,...body}=await req.json();
+    const {data,error}=await supabase
+    .from('classcodes')
+    .delete()
+    .eq('id',id)
+    .single()
+    if (error) {
+        return NextResponse.json({error:error.message},{status:500})
+    }
+    return NextResponse.json({message:'Deleted!'},{status:200})   
 }
